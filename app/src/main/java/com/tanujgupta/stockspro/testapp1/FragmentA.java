b29b44c6c4d5c393b2e8ace88eb3079edf6b6d1b
Tanuj Gupta <tanujgup@gmail.com>
2015-03-07 11:46:58 -0600

    added listfragment and inflated  its layout

--- app/src/main/java/com/tanujgupta/stockspro/testapp1/FragmentA.java
+++ app/src/main/java/com/tanujgupta/stockspro/testapp1/FragmentA.java
@@ -1,18 +1,34 @@
 package com.tanujgupta.stockspro.testapp1;
 
-import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 /**
  * Created by Tanuj on 07/03/15.
  */
-public class FragmentA extends Fragment{
+public class FragmentA extends ListFragment implements AdapterView.OnItemClickListener{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.array_of_numbers, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
